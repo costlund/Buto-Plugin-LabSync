3627fe79d0090c5c0ea81709dd8a9167eac7ebfc
costlund <claes@Mefromhd-MBP16.local>
2018-11-23 22:27:14 +0100

    Add files.

---
+++ PluginLabSync.php
@@ -0,0 +1,510 @@
+<?php
+class PluginLabSync{
+  private $files = array();
+  private $remote_host = false;
+  private $settings = null;
+  function __construct($buto) {
+    if($buto){
+      /**¨
+       * ¨Include.
+       */
+      wfPlugin::includeonce('wf/form_v2');
+      wfPlugin::includeonce('wf/array');
+      wfPlugin::includeonce('wf/yml');
+      /**
+       * Enable.
+       */
+      wfPlugin::enable('wf/bootstrap');
+      wfPlugin::enable('form/form_v1');
+      wfPlugin::enable('wf/table');
+      wfPlugin::enable('wf/ajax');
+      wfPlugin::enable('wf/bootstrapjs');
+      wfPlugin::enable('wf/dom');
+      wfPlugin::enable('wf/callbackjson');
+      wfPlugin::enable('datatable/datatable_1_10_16');
+      wfPlugin::enable('element/iframe_v1');
+      wfPlugin::enable('wf/embed');
+      /**
+       * Only webmaster if not reading files.
+       */
+      if(wfGlobals::get('method')!='files' && wfGlobals::get('method')!='upload_capture' && wfGlobals::get('method')!='delete_remote_do'  && wfGlobals::get('method')!='download_capture' && !wfUser::hasRole('webmaster')){
+        exit('Role issue says PluginLabSync.');
+      }
+      /**
+       * Layout path.
+       */
+      wfArray::set($GLOBALS, 'sys/layout_path', '/plugin/lab/sync/layout');
+      /**
+       * Memory.
+       */
+      ini_set('memory_limit', '2048M');
+      /**
+       * Time.
+       */
+      ini_set('max_execution_time', 120);
+      /**
+       * Settings.
+       */
+      $this->settings = new PluginWfArray(wfArray::get($GLOBALS, 'sys/settings/plugin_modules/'.wfArray::get($GLOBALS, 'sys/class').'/settings'));
+    }
+  }
+  /**
+   * Check ip.
+   */
+  private function check_ip(){
+    $settings = new PluginWfArray(wfPlugin::getModuleSettings());
+    $check_ip = false;
+    foreach ($settings->get('ip') as $key => $value) {
+      if($value==wfServer::getRemoteAddr()){
+        $check_ip = true;
+        break;
+      }
+    }
+    return $check_ip;
+  }
+  /**
+   * Read files from remote host.
+   */
+  public function page_files(){
+    $this->remote_host = true;
+    $check_ip = $this->check_ip();
+    if(!$check_ip){
+      exit(serialize(array()));
+    }else{
+      $this->set_files();
+      exit(serialize($this->files));
+    }
+  }
+  private function set_files($path = null){
+    $dir = wfGlobals::getAppDir().$path;
+    $scan = scandir($dir);
+    $web_folder_name = $this->getWebFolderName();
+    foreach ($scan as $key => $value) {
+      if(substr($value, 0, 1)=='.'){
+        continue;
+      }
+      if(is_dir($dir.'/'.$value)){
+        $this->set_files($path.'/'.$value);
+      }else{
+        $xpath = $path;
+        if(substr($path, 0, strlen($web_folder_name)+1) == '/'.$web_folder_name){
+          $xpath = '/[web_folder]'.substr($path, strlen($web_folder_name)+1);
+        }
+        $data = null;
+        if($this->remote_host){
+          $data = array('remote_size' => filesize($dir.'/'.$value), 'remote_time' => filemtime($dir.'/'.$value));
+        }else{
+          $data = array('local_size' => filesize($dir.'/'.$value), 'local_time' => filemtime($dir.'/'.$value));
+        }
+        $this->files[$xpath.'/'.$value] = $data;
+      }
+    }
+  }
+  private function getWebFolderName(){
+    return basename(wfGlobals::getWebDir());
+  }
+  public function page_start(){
+    wfPlugin::includeonce('wf/yml');
+    $page = new PluginWfYml(__DIR__.'/page/start.yml');
+    /**
+     * Insert admin layout from theme.
+     */
+    $page = wfDocument::insertAdminLayout($this->settings, 1, $page);
+    /**
+     * 
+     */
+    $json = json_encode(array('class' => wfGlobals::get('class')));
+    $page->setByTag(array('json' => 'var app = '.$json));
+    /**
+     * Insert admin layout from theme.
+     */
+    wfDocument::mergeLayout($page->get());
+  }
+  private function match_wildcard( $wildcard_pattern, $haystack ) {
+     $regex = str_replace(
+       array("\*", "\?"), // wildcard chars
+       array('.*','.'),   // regexp chars
+       preg_quote($wildcard_pattern)
+     );
+     return preg_match('#^'.$regex.'$#is', $haystack);
+  }  
+  public function page_read(){
+    wfPlugin::includeonce('wf/array');
+    wfPlugin::includeonce('wf/yml');
+    $settings = new PluginWfArray(wfPlugin::getModuleSettings());
+    $element = new PluginWfYml(__DIR__.'/element/read.yml');
+    $element->setByTag(array('settings' => wfHelp::getYmlDump($settings->get())));
+    /**
+     * Local files.
+     */
+    $this->set_files();
+    $local_files = $this->files;
+    /**
+     * Remote files.
+     */
+    $url = $this->getUrl('files');
+    $content = @file_get_contents($url);
+    if($content === false){
+      exit("Error when call url $url!");
+    }
+    $remote_files = @unserialize($content);
+    if($remote_files === false){
+      wfHelp::yml_dump($content, true);
+      exit("Content from url $url could not be handled!");
+    }
+    /**
+     * Merge existing remote to local.
+     */
+    foreach ($local_files as $key => $value) {
+      if(isset($remote_files[$key])){
+        $local_files[$key]['exist'] = 'both';
+        $local_files[$key]['remote_size'] = $remote_files[$key]['remote_size'];
+        $local_files[$key]['remote_time'] = $remote_files[$key]['remote_time'];
+      }else{
+        $local_files[$key]['exist'] = 'local';
+      }
+    }
+    /**
+     * Merge not existing to local.
+     */
+    foreach ($remote_files as $key => $value) {
+      if(isset($local_files[$key])){
+        //
+      }else{
+        $local_files[$key]['exist'] = 'remote';
+        $local_files[$key]['remote_size'] = $remote_files[$key]['remote_size'];
+        $local_files[$key]['remote_time'] = $remote_files[$key]['remote_time'];
+      }
+    }
+    /**
+     * Set size diff.
+     */
+    foreach ($local_files as $key => $value){
+      $item = new PluginWfArray($value);
+      if($item->get('local_size') && $item->get('remote_size') && $item->get('local_size') != $item->get('remote_size')){
+        $local_files[$key]['size_diff'] = 'sizediff';
+      }else{
+        $local_files[$key]['size_diff'] = '';
+      }
+    }
+    /**
+     * Filter.
+     */
+    foreach ($local_files as $key => $value) {
+      $local_files[$key]['allow'] = false;
+      if($settings->get('filter/item')){
+        foreach ($settings->get('filter/item') as $key2 => $value2) {
+          if($this->match_wildcard($value2['value'], $key)>0){
+            $local_files[$key]['allow'] = true;
+            continue;
+          }
+        }
+      }
+    }
+    /**
+     * Time diff.
+     */
+    foreach ($local_files as $key => $value){
+      $item = new PluginWfArray($value);
+      if($item->get('local_time') && $item->get('remote_time') && $item->get('local_time') != $item->get('remote_time')){
+        $local_files[$key]['time_diff'] = 'timediff';
+      }else{
+        $local_files[$key]['time_diff'] = '';
+      }
+    }
+    /**
+     * Local newer.
+     */
+    foreach ($local_files as $key => $value){
+      $item = new PluginWfArray($value);
+      if($item->get('local_time') && $item->get('remote_time')){
+        if($item->get('local_time') > $item->get('remote_time') && $item->get('local_time') > strtotime($settings->get('local_time'))){
+          $local_files[$key]['local_newer'] = 'local_newer';
+        }elseif($item->get('local_time') < $item->get('remote_time')){
+        #}elseif($item->get('local_time') < $item->get('remote_time') && $item->get('remote_time') > strtotime($settings->get('local_time'))){
+          $local_files[$key]['local_newer'] = 'remote_newer';
+        }else{
+          $local_files[$key]['local_newer'] = '';
+        }
+      }else{
+        $local_files[$key]['local_newer'] = '';
+      }
+    }
+    /**
+     * 
+     */
+    $tbody = array();
+    $i=0;
+    foreach ($local_files as $key => $value) {
+      $item = new PluginWfArray($value);
+      if(!$item->get('allow')){
+        continue;
+      }
+      $i++;
+      if($item->get('exist')=='local'){
+      }
+      if($item->get('exist')=='remote'){
+        $onclick = "PluginLabSync.download(this)";
+        $glyphicon = 'download';
+        $title = 'Download file from server.';
+      }else{
+        $onclick = "PluginLabSync.upload(this)";
+        $glyphicon = 'upload';
+        $title = 'Upload file to server.';
+      }
+      $tbody[] = wfDocument::createHtmlElement('tr', array(
+       wfDocument::createHtmlElement('td', $i),
+       wfDocument::createHtmlElement('td', array(
+         wfDocument::createHtmlElement('text', $key), 
+         wfDocument::createHtmlElement('a', array(wfDocument::createHtmlElement('span', null, array('class' => 'glyphicon glyphicon-'.$glyphicon))), array('onclick' => $onclick, 'title' => $title, 'class' => 'btn_upload', 'data-file' => urlencode($key), 'data-exist' => $item->get('exist'))),
+         wfDocument::createHtmlElement('a', array(wfDocument::createHtmlElement('span', null, array('class' => 'glyphicon glyphicon-trash'))), array('onclick' => "PluginLabSync.delete_form(this)", 'title' => 'Delete file.', 'class' => '', 'data-file' => urlencode($key), 'data-exist' => $item->get('exist')))
+         )),
+       wfDocument::createHtmlElement('td', '('.$item->get('exist').')', array('class' => 'td_exist')),
+       wfDocument::createHtmlElement('td', ($item->get('size_diff')?'('.$item->get('size_diff').')':null) ),
+       wfDocument::createHtmlElement('td', $item->get('local_size')),
+       wfDocument::createHtmlElement('td', $item->get('remote_size')),
+       wfDocument::createHtmlElement('td', ($item->get('local_time')?date('ymd H:i:s', $item->get('local_time')):null) ),
+       wfDocument::createHtmlElement('td', ($item->get('remote_time')?date('ymd H:i:s', $item->get('remote_time')):null) ),
+       wfDocument::createHtmlElement('td',   ($item->get('time_diff')?'('.$item->get('time_diff').')':null) ),
+       wfDocument::createHtmlElement('td', ($item->get('local_newer')?'('.$item->get('local_newer').')':null), array('class' => 'td_local_newer'))
+      ));
+    }
+    $element->setByTag(array('tbody' => $tbody));
+    wfDocument::renderElement($element->get());
+    /**
+     * Count.
+     */
+    $local_count = 0;
+    $remote_count = 0;
+    $local_newer_count = 0;
+    foreach ($local_files as $key => $value) {
+      if($value['allow'] && $value['exist']=='local'){
+        $local_count++;
+      }
+      if($value['allow'] && $value['exist']=='remote'){
+        $remote_count++;
+      }
+      if($value['allow'] && $value['local_newer']=='local_newer'){
+        $local_newer_count++;
+      }
+    }
+    $script = array();
+    $script[] = wfDocument::createHtmlElement('script', "document.getElementById('badge_local').innerHTML='$local_count';");
+    $script[] = wfDocument::createHtmlElement('script', "document.getElementById('badge_remote').innerHTML='Remote: $remote_count';");
+    $script[] = wfDocument::createHtmlElement('script', "document.getElementById('badge_local_newer').innerHTML='$local_newer_count';");
+    wfDocument::renderElement($script);
+  }
+  /**
+   * Get data for uploading purpose.
+   */
+  private function getUploadData(){
+    $data = new PluginWfArray();
+    $filename = wfRequest::get('key');
+    $data->set('filename', $filename);
+    $filename = $this->replaceWebDir($filename);
+    $data->set('content', file_get_contents(wfGlobals::getAppDir().$filename));
+    return $data;
+  }
+  /**
+   * Get url.
+   */
+  private function getUrl($method){
+    $settings = new PluginWfArray(wfPlugin::getModuleSettings());
+    return $settings->get('url').'/'.$method;
+  }
+  /**
+   * Save files from remote server.
+   * This method support multiple files.
+   */
+  public function page_upload_capture(){
+    /**
+     * Result.
+     */
+    $result = new PluginWfArray(array('success' => true, 'files' => null, 'message' => null));
+    /**
+     * Check IP.
+     */
+    $check_ip = $this->check_ip();
+    if(!$check_ip){
+      $result->set('success', false);
+      $result->set('message', 'Your ip is not registred!');
+      exit(serialize($result->get()));
+    }
+    /**
+     * Files.
+     */
+    $files = wfRequest::getAll();
+    try {
+      /**
+       * Save files and set size.
+       */
+      foreach ($files as $key => $value) {
+        $filename = $value['filename'];
+        $filename = $this->replaceWebDir($filename);
+        /**
+         * Create dir if not exist.
+         */
+        $dirname = dirname(wfGlobals::getAppDir().$filename);
+        if(!wfFilesystem::fileExist($dirname)){
+          mkdir($dirname, 0777, true);
+        }
+        /**
+         * Save file.
+         */
+        $size = file_put_contents(wfGlobals::getAppDir().$filename, $value['content']);
+        unset($files[$key]['content']);
+        $files[$key]['size'] = $size;
+      }
+    }catch (Exception $e) {
+      $result->set('success', false);
+      $result->set('message', $e->getMessage());
+      exit(serialize($result->get()));
+    }
+    
+    /**
+     * Send back result.
+     */
+    $result->set('files', $files);
+    exit(serialize($result->get()));
+  }
+  /**
+   * Upload file to remote server.
+   * Method page_upload_capture() handle response.
+   */
+  public function page_upload(){
+    $data = $this->getUploadData();
+    $url = $this->getUrl('upload_capture');
+    $params = new PluginWfArray();
+    $params->set(true, array('filename' => $data->get('filename'), 'content' => $data->get('content')));
+    wfPlugin::includeonce('server/push');
+    $push = new PluginServerPush();
+    $result = $push->push($url, $params->get());
+    exit(json_encode(unserialize($result)));
+  }
+  private function replaceWebDir($filename){
+    return str_replace('[web_folder]', $this->getWebFolderName(), $filename);
+  }
+  /**
+   * Download file client side.
+   */
+  public function page_download(){
+    /**
+     * Ask server for file.
+     */
+    $filename = wfRequest::get('key');
+    $url = $this->getUrl('download_capture');
+    wfPlugin::includeonce('server/push');
+    $push = new PluginServerPush();
+    $result = $push->push($url, array('filename' => $filename));
+    $result = new PluginWfArray(unserialize($result));
+    if($result->get('success')){
+      $filename = $this->replaceWebDir($filename);
+      /**
+       * Create dir if not exist.
+       */
+      $dirname = dirname(wfGlobals::getAppDir().$filename);
+      if(!wfFilesystem::fileExist($dirname)){
+        mkdir($dirname, 0777, true);
+      }
+      /**
+       * Save file.
+       */
+      $size = file_put_contents(wfGlobals::getAppDir().$filename, $result->get('content'));
+      $result->set('content', null);
+      $result->set('size', $size);
+    }
+    exit(json_encode($result->get()));
+  }
+  /**
+   * Download file server side.
+   */
+  public function page_download_capture(){
+    $filename = wfRequest::get('filename');
+    $filename = $this->replaceWebDir($filename);
+    /**
+     * Result.
+     */
+    $result = new PluginWfArray(array('success' => true, 'filename' => $filename, 'message' => null, 'content' => null));
+    /**
+     * Check IP.
+     */
+    $check_ip = $this->check_ip();
+    if(!$check_ip){
+      $result->set('success', false);
+      $result->set('message', 'Your ip is not registred!');
+      exit(serialize($result->get()));
+    }
+    /**
+     * Get content and put in serialize array.
+     */
+    $content = file_get_contents(wfGlobals::getAppDir().$filename);
+    $result->set('content', $content);
+    exit(serialize($result->get()));
+  }
+  public function page_delete_local(){
+    $filename = wfRequest::get('key');
+    $filename = $this->replaceWebDir($filename);
+    /**
+     * Result.
+     */
+    $result = new PluginWfArray(array('success' => true, 'filename' => $filename, 'message' => null));
+    if($this->check_ip()){
+      if(wfFilesystem::fileExist(wfGlobals::getAppDir().$filename)){
+        wfFilesystem::delete(wfGlobals::getAppDir().$filename);
+      }else{
+        $result->set('success', false);
+        $result->set('message', 'File does not exist local.');
+      }
+    }
+    exit(json_encode($result->get()));
+  }
+  /**
+   * S2C request.
+   * Call this from local server.
+   */
+  public function page_delete_remote(){
+    $filename = wfRequest::get('key');
+    $url = $this->getUrl('delete_remote_do');
+    $params = new PluginWfArray();
+    $params->set('filename', $filename);
+    wfPlugin::includeonce('server/push');
+    $push = new PluginServerPush();
+    $result = $push->push($url, $params->get());
+    $unserialize = @unserialize($result);
+    if($unserialize===false){
+      exit($result);
+    }else{
+      exit(json_encode($unserialize));
+    }
+  }
+  /**
+   * S2S request.
+   */
+  public function page_delete_remote_do(){
+    $filename = wfRequest::get('filename');
+    $filename = $this->replaceWebDir($filename);
+    /**
+     * Result.
+     */
+    $result = new PluginWfArray(array('success' => true, 'filename' => $filename, 'message' => null));
+    /**
+     * Check IP.
+     */
+    $check_ip = $this->check_ip();
+    if(!$check_ip){
+      $result->set('success', false);
+      $result->set('message', 'Your ip is not registred!');
+      exit(serialize($result->get()));
+    }
+    /**
+     * Get content and put in serialize array.
+     */
+    if(wfFilesystem::fileExist(wfGlobals::getAppDir().$filename)){
+      wfFilesystem::delete(wfGlobals::getAppDir().$filename);
+    }else{
+      $result->set('success', false);
+      $result->set('message', 'File does not exist remote.');
+    }
+    exit(serialize($result->get()));
+  }
+}
---
+++ js/function.js
@@ -0,0 +1,129 @@
+function PluginLabSync(){
+  this.btn_delete = null;
+  this.upload = function(btn){
+    var local_newer = btn.parentNode.parentNode.getElementsByClassName('td_local_newer')[0];
+    local_newer.innerHTML = '<img src="/plugin/wf/ajax/loading.gif">';
+    $.get( "upload?key="+btn.getAttribute('data-file'), function( data ) {
+      console.log(data);
+      data = JSON.parse(data);
+      if(data.success){
+        var span = btn.getElementsByClassName('glyphicon')[0];
+        span.style.color='green';
+        $(span).removeClass('glyphicon-upload');
+        $(span).addClass('glyphicon-cloud-upload');
+        local_newer.innerHTML = 'uploaded';
+      }else{
+        alert(data.message);
+      }
+    });    
+  }
+  this.upload_all_localnewer = function(btn){
+    if(!confirm('Upload all visible where Local newer is (localnewer)?')){
+      return null;
+    }
+    var local_newer = document.getElementById('sync_table_wrapper').getElementsByClassName('td_local_newer');
+    var count = 0;
+    for(i=0;i<local_newer.length;i++){
+      if(local_newer[i].innerHTML=='(local_newer)'){
+        var b = local_newer[i].parentNode.getElementsByClassName('btn_upload')[0];
+        b.onclick();
+        count++;
+      }
+    }
+    console.log(count+' files uploaded.');
+  }
+  this.upload_all_exist_local = function(btn){
+    if(!confirm('Upload all visible where Exist is (local)?')){
+      return null;
+    }
+    var local = document.getElementById('sync_table_wrapper').getElementsByClassName('td_exist');
+    var count = 0;
+    for(i=0;i<local.length;i++){
+      if(local[i].innerHTML=='(local)'){
+        var b = local[i].parentNode.getElementsByClassName('btn_upload')[0];
+        b.onclick();
+        count++;
+      }
+    }
+    console.log(count+' files uploaded.');
+  }
+  this.download = function(btn){
+    var local_newer = btn.parentNode.parentNode.getElementsByClassName('td_local_newer')[0];
+    local_newer.innerHTML = '<img src="/plugin/wf/ajax/loading.gif">';
+    $.get( "download?key="+btn.getAttribute('data-file'), function( data ) {
+      console.log(data);
+      data = JSON.parse(data);
+      if(data.success){
+        var span = btn.getElementsByClassName('glyphicon')[0];
+        span.style.color='green';
+        $(span).removeClass('glyphicon-upload');
+        $(span).addClass('glyphicon-cloud-upload');
+        local_newer.innerHTML = 'downloaded';
+      }else{
+        alert(data.message);
+      }
+    });    
+  }
+  this.delete_form = function(btn){
+    this.btn_delete = btn;
+    PluginWfBootstrapjs.modal({id: 'modal_delete', content: '', lable: 'Delete'});
+    var content = 
+            [
+      {type: 'p', innerHTML: [{type: 'strong', innerHTML: 'File: '},{type: 'span', innerHTML: decodeURIComponent(btn.getAttribute('data-file'))}]},
+      {type: 'p', innerHTML: [{type: 'strong', innerHTML: 'Exist: '},{type: 'span', innerHTML: btn.getAttribute('data-exist')}]},
+      {type: 'p', innerHTML: [
+          {type: 'a', attribute: {class: 'btn btn-primary btn-sm', id: 'btn_delete_local', onclick: "PluginLabSync.delete('local')"}, innerHTML: 'Delete local'},
+          {type: 'a', attribute: {class: 'btn btn-warning', id: 'btn_delete_both', onclick: "PluginLabSync.delete('both')"}, innerHTML: 'Delete both'},
+          {type: 'a', attribute: {class: 'btn btn-primary btn-sm', id: 'btn_delete_remote', onclick: "PluginLabSync.delete('remote')"}, innerHTML: 'Delete remote'}
+        ], attribute: {style: 'text-align:center'}}
+            ];
+    PluginWfDom.render(content, document.getElementById('modal_delete_body'));
+    if(btn.getAttribute('data-exist')!='both'){
+      document.getElementById('btn_delete_both').setAttribute('disabled', true);
+      document.getElementById('btn_delete_both').setAttribute('onclick', null);
+    }
+    if(btn.getAttribute('data-exist')=='local'){
+      document.getElementById('btn_delete_remote').setAttribute('disabled', true);
+      document.getElementById('btn_delete_remote').setAttribute('onclick', null);
+    }
+    if(btn.getAttribute('data-exist')=='remote'){
+      document.getElementById('btn_delete_local').setAttribute('disabled', true);
+      document.getElementById('btn_delete_local').setAttribute('onclick', null);
+    }
+  }
+  this.delete = function(mode){
+    var local_newer = this.btn_delete.parentNode.parentNode.getElementsByClassName('td_local_newer')[0];
+    local_newer.innerHTML = '<img src="/plugin/wf/ajax/loading.gif">';
+    if(mode=='local'){
+      $.get( "delete_local?key="+this.btn_delete.getAttribute('data-file')+'&mode='+mode, function( data ) {
+        data = JSON.parse(data);
+        if(data.success){
+          local_newer.innerHTML = 'Local deleted';
+          $('#modal_delete').modal('hide');
+        }else{
+          local_newer.innerHTML = data.message;
+        }
+      });
+    }else if(mode=='remote'){
+      $.get( "delete_remote?key="+this.btn_delete.getAttribute('data-file'), function( data ) {
+        data = JSON.parse(data);
+        if(data.success){
+          local_newer.innerHTML = 'Remote deleted';
+          $('#modal_delete').modal('hide');
+        }else{
+          local_newer.innerHTML = data.message;
+        }
+      });
+    }else if(mode=='both'){
+      alert('not yet');
+    }
+  }
+}
+var PluginLabSync = new PluginLabSync();
+
+
+
+
+
+
+
